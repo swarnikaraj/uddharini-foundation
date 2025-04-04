810d1b0fb711b8de58c87b173c35b8b65a2e2d5c
Swarnikaraj <swarnikarajsingh@gmail.com>
2025-04-04 23:45:37 +0530

    dhsd

--- landingpage/postcss.config.mjs
+++ landingpage/postcss.config.mjs
@@ -1,23 +1,4 @@
-module.exports = {
+const config = {
   plugins: ["@tailwindcss/postcss"],
-  content: ["./src/**/*.{js,ts,jsx,tsx}"], // Adjust paths as needed
-  theme: {
-    extend: {
-      animation: {
-        "fade-in": "fadeIn 1.5s ease-in-out",
-        "slide-in": "slideIn 1s ease-in-out",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
-        slideIn: {
-          "0%": { transform: "translateY(20px)", opacity: 0 },
-          "100%": { transform: "translateY(0)", opacity: 1 },
-        },
-      },
-    },
-  },
-  plugins: [],
 };
+export default config;
---
+++ landingpage/tailwind.config.js
@@ -0,0 +1,23 @@
+export default config = {
+  plugins: ["@tailwindcss/postcss"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // Adjust paths as needed
+  theme: {
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 1.5s ease-in-out",
+        "slide-in": "slideIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        slideIn: {
+          "0%": { transform: "translateY(20px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
